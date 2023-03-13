12254c5e91d6891c1b975226c6142b5cbce46ec4
Yee-Q <1051195623@qq.com>
2023-03-14 00:34:20 +0800

    filter异常处理

--- src/main/java/com/styeeqan/community/web/filter/WrapperFilter.java
+++ src/main/java/com/styeeqan/community/web/filter/WrapperFilter.java
@@ -1,6 +1,7 @@
 package com.styeeqan.community.web.filter;
 
 import com.styeeqan.community.common.http.request.RequestWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -8,13 +9,20 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@Slf4j
 @Component
 @WebFilter(urlPatterns = "/**")
 public class WrapperFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // 防止流读取一次就没了,将流继续传递下去
-        filterChain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) {
+        HttpServletRequest requestWrapper = null;
+        try {
+            // 防止流读取一次就没了,将流继续传递下去
+            requestWrapper = new RequestWrapper((HttpServletRequest) request);
+            filterChain.doFilter(requestWrapper, response);
+        } catch (Exception e) {
+            log.error("[WrapperFilter-Error] uri:{}", requestWrapper.getRequestURI(), e);
+        }
     }
 }
